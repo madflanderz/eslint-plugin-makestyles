037407dca1bc03b2182b324285730e5d85c57087
Martin Anders <martin.anders.external@zalando.de>
2020-01-22 18:27:41 +0100

    Fix: position of name prop in second argument is not important anymore.

--- tests/lib/rules/makestyles-with-name.js
+++ tests/lib/rules/makestyles-with-name.js
@@ -19,12 +19,24 @@ var ruleTester = new RuleTester();
 ruleTester.run("makestyles-with-name", rule, {
   valid: [
     // give me some code that won't trigger a warning
-    "var useStyles = makeStyles( { root: { color: 'red'} }, { name: 'MyComponent' })"
+    "var useStyles = makeStyles( { root: { color: 'red'} }, { name: 'MyComponent' })",
+    "var useStyles = makeStyles( { root: { color: 'red'} }, { otherProps: 'value', name: 'MyComponent' })",
+    "var useStyles = makeStyles( { root: { color: 'red'} }, { name: 'MyComponent', otherProps: 'value' })"
   ],
 
   invalid: [
     {
       code: "var useStyles = makeStyles({})",
+      errors: [
+        {
+          message:
+            'No name for style defined. Please add {name: "ComponentName"} as second argument.',
+          type: "VariableDeclaration"
+        }
+      ]
+    },
+    {
+      code: "var useStyles = makeStyles({}, { names: 'Name' })",
       errors: [
         {
           message:
